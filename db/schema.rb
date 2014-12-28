4a35d42b6f17dda2e250383b8d643a6e0a652fcb
Konstantinos Christofilos <k.christofilos@intelen.com>
2014-12-28 18:02:59 +0200

    Added microposts and relations between them and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141228142128) do
+ActiveRecord::Schema.define(:version => 20141228155118) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "title"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
