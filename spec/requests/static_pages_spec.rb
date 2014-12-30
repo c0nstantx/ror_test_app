c38873856097b34351c16a9b74e00e9265e48583
Konstantinos Christofilos <k.christofilos@intelen.com>
2014-12-30 17:17:49 +0200

    A little bit of theming and routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,20 +4,20 @@ describe "Static pages" do
   let(:title) { "Ruby on Rails Tutorial Sample App | #{$subtitle}" }
 
   describe "Home page" do
-    it "should have the h1 'Sample All'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+    it "should have the h1 'my First RoR App'" do
+      visit root_path
+      page.should have_selector('h1', :text => 'my First RoR App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
         :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       $subtitle = "Home"
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title',
         :text => $subtitle)
     end
@@ -25,13 +25,13 @@ describe "Static pages" do
 
   describe "Help page" do
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the right title" do
       $subtitle = "Help"
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
         :text => title)
     end
@@ -39,13 +39,13 @@ describe "Static pages" do
 
   describe "About page" do
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title" do
       $subtitle = "About Us"
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
         :text => title)
     end
@@ -53,13 +53,13 @@ describe "Static pages" do
 
   describe "Contact page" do
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the right title" do
       $subtitle = "Contact"
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
         :text => title)
     end
