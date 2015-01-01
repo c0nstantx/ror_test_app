e747a3c36c6da8bd3a0fed7ec9118c1a5e0ab675
Konstantinos Christofilos <k.christofilos@intelen.com>
2015-01-01 13:54:11 +0200

    Fix signup error message for password digest. Add some more tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
-  def gravatar_for user, params = {}
+  def gravatar_for user, options = { size: 50 }
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_size = params[:size] ? "?s=#{params[:size]}" : ""
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png#{gravatar_size}"
+    gravatar_size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{gravatar_size}"
     image_tag gravatar_url, alt: user.name, class: "gravatar"
   end
 end
